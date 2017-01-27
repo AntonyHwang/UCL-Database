d26bca8fef1f0d689bedd37e4cf36e87f85d24aa
Allen Wang <allen.wang@yale.edu>
2017-01-27 13:57:11 +0000

    modified create table script

--- blogster/createtable.php
+++ blogster/createtable.php
@@ -1,84 +1,280 @@
 <?php
-//DB connection info
+//To test on your local machine, modify your MAMP/MySQL user/pwd
 $host = "eu-cdbr-azure-west-a.cloudapp.net";
 $user = "bd38b99b177044";
 $pwd = "5e59f1c8";
 $db = "blogster";
+
+
 try {
-    $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+    $conn = new PDO( "mysql:host=$host", $user, $pwd);
+    echo "Connected Successfully";
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    $sql = "CREATE TABLE users(
-	    id_user INT NOT NULL AUTO_INCREMENT,
-	    PRIMARY KEY(id_user),
-	    title VARCHAR(10),
-	    first_Name VARCHAR(30),
-	    last_Name VARCHAR(30),
-	    email VARCHAR(30),
-	    password VARCHAR(30))";
-    $conn->query($sql);
-        
-    $sql = "CREATE TABLE friendships( 
-	    id_friendship INT NOT NULL AUTO_INCREMENT),
-        ADD FOREIGN KEY (id_user1) REFERENCES users(id_user),
-        ADD FOREIGN KEY (id_user2) REFERENCES users(id_user))";
-    $conn->query($sql);
-        
-    $sql = "CREATE TABLE friend_request(
-   	    id _request INT NOT NULL AUTO_INCREMENT,
-	    PRIMARY KEY(id_request),
-        ADD FOREIGN KEY (id_from_user) REFERENCES users(id_user),
-        ADD FOREIGN KEY (id_to_user) REFERENCES users(id_user))";
-    $conn->query($sql);
-    
-    $sql = "CREATE TABLE posts(
-	    id_post INT NOT NULL AUTO_INCREMENT,
-	    timestamp TIMESTAMP,
-        body VARCHAR(150),
-	    privacy_setting INT,
-        ADD FOREIGN KEY (id_user) REFERENCES users(id_user))";
-    $conn->query($sql);
-        
-    $sql = "CREATE TABLE circles(
-	    id_circle INT NOT NULL AUTO_INCREMENT,
-	    name  VARCHAR(30),
-        ADD FOREIGN KEY (id_owner) REFERENCES users(id_user))";
-    $conn->query($sql);
-    
-    $sql = "CREATE TABLE members(
-	    id_member INT NOT NULL AUTO_INCREMENT),
-        ADD FOREIGN KEY (id_circle) REFERENCES circles(id_circle),
-        ADD FOREIGN KEY (id_user) REFERENCES users(id_user))";
-    $conn->query($sql);
-        
-    $sql = "CREATE TABLE messages(
-	    id_message INT NOT NULL AUTO_INCREMENT,
-	    date_time TIMESTAMP,
-	    body VARCHAR(150),
-        ADD FOREIGN KEY (id_circle) REFERENCES circles(id_circle))";
-    $conn->query($sql);
+    $sql = '
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Jan 26, 2017 at 04:21 PM
+-- Server version: 5.6.34
+-- PHP Version: 7.1.0
 
-    $sql = "CREATE TABLE photos(   
-        id_photo INT NOT NULL AUTO_INCREMENT,
-        date_time TIMESTAMP,
-        body VARCHAR(150),
-        privacy_setting INT,
-        ADD FOREIGN KEY (id_user) REFERENCES users(id_user))";
-    $conn->query($sql);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-    $sql = "CREATE TABLE comments(
-	Id_comment INT NOT NULL AUTO_INCREMENT,
-	date_time TIMESTAMP,
-	body VARCHAR(150),
-    ADD FOREIGN KEY (id_photo) REFERENCES photos(id_photo))";
-    $conn->query($sql);
+--
+-- Database: `blogster`
+--
+CREATE DATABASE IF NOT EXISTS `blogster` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `blogster`;
 
-    $sql = "CREATE TABLE Admins(
-        id_admin INT NOT NULL AUTO_INCREMENT,
-        title VARCHAR(10),
-	    first_name VARCHAR(30),
-        last_name VARCHAR(30),
-        email VARCHAR(30),
-        password VARCHAR(30))";
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(10) NOT NULL,
+  `title` varchar(10) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `circle`
+--
+
+CREATE TABLE `circle` (
+  `id_circle` int(10) NOT NULL,
+  `id_user` int(10) NOT NULL,
+  `name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id_comment` int(10) NOT NULL,
+  `id_photo` int(10) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `body` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friendrequest`
+--
+
+CREATE TABLE `friendrequest` (
+  `id_request` int(10) NOT NULL,
+  `id_from_user` int(10) NOT NULL,
+  `id_to_user` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friendship`
+--
+
+CREATE TABLE `friendship` (
+  `id_user` int(10) NOT NULL,
+  `id_friend` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `id_member` int(10) NOT NULL,
+  `id_circle` int(10) NOT NULL,
+  `id_user` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `id_message` int(11) NOT NULL,
+  `id_circle` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `body` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `photo`
+--
+
+CREATE TABLE `photo` (
+  `id_photo` int(10) NOT NULL,
+  `id_user` int(10) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `body` varchar(150) NOT NULL,
+  `privacy_setting` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE `post` (
+  `id_post` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `body` varchar(150) NOT NULL,
+  `privacy_setting` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(10) NOT NULL,
+  `title` varchar(10) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `circle`
+--
+ALTER TABLE `circle`
+  ADD PRIMARY KEY (`id_circle`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id_comment`);
+
+--
+-- Indexes for table `friendrequest`
+--
+ALTER TABLE `friendrequest`
+  ADD PRIMARY KEY (`id_request`);
+
+--
+-- Indexes for table `friendship`
+--
+ALTER TABLE `friendship`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id_member`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id_message`);
+
+--
+-- Indexes for table `photo`
+--
+ALTER TABLE `photo`
+  ADD PRIMARY KEY (`id_photo`);
+
+--
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id_post`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `circle`
+--
+ALTER TABLE `circle`
+  MODIFY `id_circle` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id_comment` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `friendrequest`
+--
+ALTER TABLE `friendrequest`
+  MODIFY `id_request` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `id_member` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id_message` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `photo`
+--
+ALTER TABLE `photo`
+  MODIFY `id_photo` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT;
+  '
+;
     $conn->query($sql);
 }
 catch(Exception $e){
