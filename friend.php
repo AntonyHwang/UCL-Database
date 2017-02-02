4dd644c5c94bde7ec5a54aebd6be6c165ae64767
uclhenry <yuhangxuhenry@gmail.com>
2017-02-02 16:38:26 +0000

    homepage connect database

--- friend.php
+++ friend.php
@@ -23,19 +23,51 @@ div {
 <input></input>
 <button>search</button>
 <?php
-//echo "Hello World!";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "blogster";
 
-?>
-<?php 
-for ($x = 0; $x <= 10; $x++) {?>
-	<div class = 'friend'>
-	 
-	 <p>friend<p>
-	 <p></p>
-	</div>
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
-<?php } 
+$sql = "SELECT id_user, id_friend FROM friendship";
+$result = $conn->query($sql);
 ?>
+
+
+<?php
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+		?>
+		<div class = 'friend'>
+
+		<p><p>		
+		<?php
+
+		
+        echo "id:" . $row["id_user"]. "</br> friend: " . $row["id_friend"]. "<br>";
+		echo "time";
+		echo "</br></br>";		
+		?>
+		
+		
+		<?php
+
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+
+
+
 </div>
 <div class = 'recm'>
 <h1>recommendation</h1>
