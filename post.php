de6aaba500e06e29bdd570d406f81898e99712ff
uclhenry <yuhangxuhenry@gmail.com>
2017-02-02 15:55:11 +0000

    connect database

--- post.php
+++ post.php
@@ -16,6 +16,34 @@ div {
 //echo "Hello World!";
 
 ?>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "blogster";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT id_post, id_user, body FROM post";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id_post:" . $row["id_post"]. "</br> userid: " . $row["id_user"]. "</br>body " . $row["body"]. "<br>";
+		echo "comment";
+		echo "</br></br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
 <?php 
 for ($x = 0; $x <= 10; $x++) {?>
 	<div class = 'post'>
