3ad7787047a38e47230ae545f052d62fb9af9ba7
uclhenry <yuhangxuhenry@gmail.com>
2017-02-02 16:48:21 +0000

    id change

--- friend.php
+++ friend.php
@@ -34,8 +34,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
-$sql = "SELECT id_user, id_friend FROM friendship";
+$thisid = 123;
+$sql = "SELECT * FROM `friendship` WHERE `id_user` =".$thisid;
 $result = $conn->query($sql);
 ?>
 
