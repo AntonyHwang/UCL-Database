a747f0dba4f5057aa532ef918a8c69f4c8718c1c
uclhenry <yuhangxuhenry@gmail.com>
2017-02-13 23:20:17 +0000

    Add files via upload

--- randomfriendship.php
+++ randomfriendship.php
@@ -1,8 +1,8 @@
 <?php
-$servername ="localhost";
-$username ="root";
-$password ="";
-$dbname ="blogster";
+            $host = "eu-cdbr-azure-west-a.cloudapp.net";
+            $user = "bd38b99b177044";
+            $pwd = "5e59f1c8";
+            $db = "blogster";
 ?>
 <?php
 $start = 0;
@@ -15,33 +15,39 @@ for($i = 0;$i <1000;$i++){
 	$num1  = rand($start, $end);
 	$num2  = rand($start, $end);
     $fship = [$num1,$num2];	
-	while(in_array($fship, $ranlist) ){
+	$fship2 = [$num1,$num2];	
+	while(in_array($fship, $ranlist) or in_array($fship2, $ranlist)){
 	$num1  = rand($start, $end);
 	$num2  = rand($start, $end);
     $fship = [$num1,$num2];	
 	}
 	array_push($ranlist,$fship);
-	array_push($ranlist,[$fship[1],$fship[0]]);
+	//array_push($ranlist,[$fship[1],$fship[0]]);
 }
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed:". $conn->connect_error);
-} 
+            try {
+                $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+                $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+				echo 'ok';
+            }
+            catch(Exception $e){
+                die(var_dump($e));
+            }
+			
 foreach ($ranlist as $line){
 	
 	
 $sql = "INSERT INTO friendship (id_user, id_friend)
 VALUES ('$line[0]', '$line[1]')";
-if ($conn->query($sql) === TRUE) {
-    echo"New record created successfully";
-} else {
-    echo"Error:". $sql ."<br>". $conn->error;
+$stmt = $conn->query($sql);  
+	if (!$stmt){
+		die('new friendship failed');
+		}
+	else {
+	echo"New friendship added successfully<br>";
+	}
+
 }
 
 
-}
-
-$conn->close();
 ?>
