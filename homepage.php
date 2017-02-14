7d07de9192e477299818723e114d49ea317e9303
BILL MA <ajsdnh@gmail.com>
2017-02-14 13:50:48 +0000

    Layout added&displaying profile pics

--- homepage.php
+++ homepage.php
@@ -15,6 +15,7 @@
                 </div>
             </fieldset>
         </form>
+
         <?php
             // DB connection info
             //TODO: Update the values for $host, $user, $pwd, and $db
@@ -32,11 +33,17 @@
                 die(var_dump($e));
             }
             //Insert registration info
+            $matchedFirstName=array();  
+            $matchedSurname=array();
+            $matchedId=array();
+            $matchedPassword=array();
+            $counter=0;
+
             if(!empty($_POST)) {
                 try {
                     // Retrieve data
                     $fullname = $_POST['Name'];
-                    $sql_select = "SELECT first_name, surname FROM (SELECT first_name,surname,CONCAT(first_name,' ',surname) AS 'Con_Name' FROM user) AS x WHERE Con_Name LIKE '%".$fullname."%'";
+                    $sql_select = "SELECT first_name, surname, id_user, password FROM (SELECT first_name,surname,id_user, password,CONCAT(first_name,' ',surname) AS 'Con_Name' FROM user) AS x WHERE Con_Name LIKE '%".$fullname."%'";
 
                     $stmt = $conn->query($sql_select);  
                     if (!$stmt){
@@ -44,7 +51,11 @@
                     }
                     else {
                         while($row = $stmt->fetch()){
-                            echo "First Name: ".$row["first_name"]." Surname: ".$row["surname"];
+                            $matchedFirstName[$counter] = $row[first_name];
+                            $matchedSurname[$counter] = $row[surname];
+                            $matchedId[$counter]=$row[id_user];
+                            $matchedPassword[$counter]=$row[password];
+                            $counter++;
                         }
                     }
                 }
@@ -52,7 +63,21 @@
                     die(var_dump($e));
                 }
             }
+            
         ?>
+        <h1>
+            <?php
+            for ($pos=0; $pos < $counter; $pos++) {  
+
+                echo "<img src=\"./uploads/".$matchedId[$pos].$matchedPassword[$pos]."/profile.jpg\" alt=\"Profile Pic\" style=\"width:120px;height 120px;\">";
+                echo $matchedFirstName[$pos]." ".$matchedSurname[$pos];
+                echo "<button type=\"submit\" class=\"btn btn-default\">Add Friend</button>";
+                echo "<br>";
+            }
+        ?>
+    </h1>
+
+
 
     </body>
 </html>
