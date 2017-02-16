7e58c419f5c5f1b5bf833a928d3c2bfc379c3783
Allen Wang <allen.wang@yale.edu>
2017-02-16 17:47:07 +0000

    Added Photo Viewer

--- photoPage.php
+++ photoPage.php
@@ -1,12 +1,23 @@
-<?php ob_start(); ?>
-<form action="uploadPhoto.php" method="get" >
-	<input type="hidden" name="uploadButton" value="upload">
-  	<input type="submit" value="Upload Photo">
-</form>
-
+<style>
+    div.photoList {
+        text-align: center;
+        margin: auto;
+        width: 500px;
+        border: 5px solid blue;
+    }
+    img.individualPhoto {
+        width: 420px;
+    }
+    figcaption {
+        height: 100px;
+        border: 5px solid yellow;
+    }
+</style>
 <?php 
-	require ("includes/config.php");
-	$host = "eu-cdbr-azure-west-a.cloudapp.net";
+    ob_start();
+    require ("includes/config.php");
+    include_once "header.php";
+    $host = "eu-cdbr-azure-west-a.cloudapp.net";
     $user = "bd38b99b177044";
     $pwd = "5e59f1c8";
     $db = "blogster";
@@ -18,35 +29,48 @@
     catch(Exception $e){
         die(var_dump($e));
     }
+    if (!empty($_GET)) {
         try {
-            // Retrieve data
-            $user_id = $_SESSION['id'];
+            if ($_GET['id'] == $_SESSION['id']) {
+                ?>
+                <form action="uploadPhoto.php" method="get" >
+                    <input type="hidden" name="uploadButton" value="upload">
+                    <input type="submit" value="Upload Photo">
+                </form>
+                <?php
+            }
+            $user_id = $_GET['id'];
             $sql_select = ("SELECT * FROM photo WHERE id_user = '".$user_id."' ORDER BY id_photo DESC");
             $stmt = $conn->prepare($sql_select);
             $stmt->execute();
             $results = $stmt->fetchAll();
-            echo $_SESSION['id']. " " . $_SESSION['email'].count($results);
+
+            #ADD different photo rendering for circles/friends/privacy when we decide how to do that
+
+
             ?>
-            <div>
+            <div class="photoList">
             <?php
             foreach($results as $row) {
-					?>
-  						<a href="<?php echo $row["file_path"]?>">
-    						<figure>
-      							<img src="<?php echo $row["file_path"]?>">
-      							<figcaption><?php echo $row["body"]?></figcaption>
-    						</figure>
-  						</a>
-            	<?php
-			}
-			?>
-		</div>
-		<?php
+                    $photoViewLink = "photoViewer.php?id=".$user_id."&photoPath=".$row["file_path"]."&caption=".$row["body"];
+                    ?>
+                        <a href="<?php echo $photoViewLink ?>">
+                            <figure>
+                                <img class="individualPhoto" src="<?php echo $row["file_path"]?>">
+                                <figcaption><?php echo $row["body"]?></figcaption>
+                            </figure>
+                        </a>
+                <?php
+            }
+            ?>
+        </div>
+        <?php
             print_r($results);
         }
         catch(Exception $e) {
             die(var_dump($e));
         }
+    }
     
 ?>
 
