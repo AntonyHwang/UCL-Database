084ebeff87ef4f676b974cc417a5a0655048ced1
Antony Hwang <AntonyHwang@hotmail.com>
2017-03-13 02:24:28 +0000

    export fix

--- download.php
+++ download.php
@@ -1,7 +1,7 @@
 <?php
     header('Content-Disposition: attachment; filename="'.$_GET['profile'].'.xml"');
     header('Content-type: text/xml');
-    readfile(dirname(__FILE__).'xml_export/'.$_GET['profile'].'.xml');
-    unlink(dirname(__FILE__).'xml_export/'.$_GET['profile'].'.xml');
+    readfile(dirname(__FILE__).'/xml_export/'.$_GET['profile'].'.xml');
+    unlink(dirname(__FILE__).'/xml_export/'.$_GET['profile'].'.xml');
     header("./profile.php?profile=".$_GET['profile']);
 ?>
